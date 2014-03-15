39f7df65793f0d0095035001c26091fc07aebb9f
badichiniran@gmail.com <badichiniran>
2014-03-15 17:49:18 +0200

    first

---
+++ HomeManagmentApplication/GetProductCategories.aspx.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Script.Serialization;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class GetProductCategories : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        DBservices dbs = new DBservices();
+
+        JavaScriptSerializer serializer = new JavaScriptSerializer();
+
+        DataTable ProductCategories;
+
+
+        try
+        {
+            ProductCategories = dbs.GetProductCategories();   // 1 is shabat imutz
+
+        }
+
+        catch (Exception ex)
+        {
+            Logger.writeToLog(LoggerLevel.ERROR, "page :ActivityShabatDog.aspx.cs, the exeption message is : " + ex.Message);
+            throw;
+        }
+
+        ProductCategories.Columns[0].ColumnName = "Code";
+        ProductCategories.Columns[1].ColumnName = "Name";
+
+        string jsonStringCategories = serializer.Serialize(SerializeTable(ProductCategories));
+
+
+        string jsonString = "{\"Categories\":" + jsonStringCategories + "}";
+        Response.Write(jsonString);
+        Response.End();
+
+    }
+
+
+    private IEnumerable<Dictionary<string, object>> SerializeTable(DataTable table)
+    {
+        return table.DefaultView.OfType<DataRowView>().Select(row =>
+        {
+            var result = new Dictionary<string, object>();
+            foreach (DataColumn column in table.Columns)
+            {
+                result.Add(column.ColumnName, Convert.ToString(row.Row[column.ColumnName]));
+            }
+
+            return result;
+        });
+    }
+}
---
+++ HomeManagmentApplication/InsertProduct.aspx.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class InsertProduct : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        DBservices dbs = new DBservices();
+        if (Request.QueryString == null)
+        {
+            Response.End();
+            return;
+        }
+        NameValueCollection requestQuery = Request.QueryString;
+
+        try
+        {
+            Product product = new Product()
+            {
+
+                Product_amount = requestQuery["Prodect_amount"],
+                Product_id = requestQuery["Product_id"],
+                Units = requestQuery["Units"],
+                Comment = requestQuery["Comment"],
+                UserId = requestQuery["UserId"],
+
+            };
+            dbs.insertProductToList(product);
+
+
+        }
+        catch (Exception ex)
+        {
+            if (ex.Message.Contains("AnimelType_id_FK1"))
+                Response.StatusCode = 1; // must choose AnimelType
+            else
+            {
+                Response.StatusCode = 999;  //general error
+                Logger.writeToLog(LoggerLevel.ERROR, "page :InjuredPet_Incident.aspx.cs, the exeption message is : " + ex.Message);
+                //send mail to systemsupport 
+            }
+        }
+
+    }
+}
